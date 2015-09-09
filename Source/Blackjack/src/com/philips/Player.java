ed35d80d1d129448d2ab7ab9bda7b4bb26ce9a45
Roel van Dun <roel.van.dun@philips.com>
2015-09-09 15:42:27 +0200

    assigning cards to the player

--- Source/Blackjack/src/com/philips/Player.java
+++ Source/Blackjack/src/com/philips/Player.java
@@ -1,5 +1,17 @@
 package com.philips;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Player {
+    private List<Card> cards = new ArrayList<>();
+
+    public int getCardsCount() {
+        return cards.size();
+    }
+
+    public void assignCard(Card card) {
+        cards.add(card);
+    }
 }
--- Source/Blackjack/test/com/philips/PlayerTest.java
+++ Source/Blackjack/test/com/philips/PlayerTest.java
@@ -3,11 +3,26 @@ package com.philips;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 public class PlayerTest {
 
     @Test
     public void itExists() throws Exception {
         assertNotNull(new Player());
+    }
+
+    @Test
+    public void itHasCards() throws Exception {
+        Player player = new Player();
+        assertTrue(player.getCardsCount() == 0);
+    }
+
+    @Test
+    public void itCanReceiveCard() throws Exception {
+        Player player = new Player();
+        Card card = new Card(1);
+        player.assignCard(card);
+        assertTrue(player.getCardsCount() == 1);
     }
 }

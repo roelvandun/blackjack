e87e974d1817a89b9eec64dae36efb4ba80b4c3c
Jean Herfs <jean.herfs@philips.com>
2015-09-09 14:01:24 +0200

    Created test setup and added junit.

---
+++ Source/Blackjack/test/com/philips/BlackJackTest.java
@@ -0,0 +1,13 @@
+package com.philips;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class BlackJackTest {
+
+    @Test
+    public void testTheTruthIsTold() {
+        assertTrue(true);
+    }
+}
